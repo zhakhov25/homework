b99190b37722bfe1b15e6933dd126f1e7d657700
Zhakhov25 <lexas.zhakhov@gmail.com>
2021-07-27 15:11:56 +0300

    Done second task

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Задача 1
 
 const amountFizzBuzz = num => {
-    for(let i= 1; i <= num; i++) {
+    for(let i = 1; i <= num; i++) {
         if(i % 3 === 0) {
             console.log('FizzBuzz');
         } else if(i % 2 === 0) {
@@ -14,3 +14,16 @@ const amountFizzBuzz = num => {
   }
   
 amountFizzBuzz(10);
+
+// Задача 2
+
+const factorNumber = num => {
+    let sum = 1;
+    debugger;
+    for(let i = 1; i <= num; i++) {
+        sum *= i;
+    }
+    return  sum;
+}
+
+console.log(factorNumber(10));
